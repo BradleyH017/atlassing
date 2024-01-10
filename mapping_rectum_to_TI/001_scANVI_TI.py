3cc3c05d574bbc7c602dfb72ecff72e99300e931
BradleyH017 <bh18@sanger.ac.uk>
2024-01-10 16:36:55 +0000

    Fix saving issues, fix environment

--- mapping_rectum_to_TI/001_scANVI_TI.py
+++ mapping_rectum_to_TI/001_scANVI_TI.py
@@ -10,23 +10,23 @@ __version__ = '0.0.1'
 
 # Load libraries
 import sys
-sys.path.append('/software/team152/bh18/pip')
-sys.path.append('/usr/local/')
+import pandas as pd
+#sys.path.append('/software/team152/bh18/pip')
+#sys.path.append('/usr/local/')
 print("System path")
 print(sys.path)
 import numpy as np
 print("Numpy file")
 print(np.__file__)
-import pandas as pd
 import anndata as ad
 import matplotlib as mp
 from matplotlib import pyplot as plt
 from matplotlib.pyplot import rc_context
-import kneed as kd
+#import kneed as kd
 import scvi
 import csv
 import datetime
-import seaborn as sns
+#import seaborn as sns
 import matplotlib.pyplot as plt
 import math
 import scipy.stats as st
@@ -34,13 +34,13 @@ import re
 from scipy import stats
 from sklearn.mixture import GaussianMixture
 from scipy.stats import norm
-from sympy import symbols, Eq, solve
+# from sympy import symbols, Eq, solve
 from sklearn.preprocessing import StandardScaler
 from scipy.optimize import fsolve
 from scipy.optimize import brentq
 import torch
 from pytorch_lightning import Trainer
-#scvi.settings.dl_pin_memory_gpu_training =  True
+scvi.settings.dl_pin_memory_gpu_training =  True
 from scipy import sparse
 import argparse
 print("Loaded libraries")
@@ -53,7 +53,7 @@ def parse_options():
     # Inherit options
     parser = argparse.ArgumentParser(
             description="""
-                Transfer learning of TI annotations into the rectum
+                scANVI model computation of the the reference dataset
                 """
         )
     
@@ -94,9 +94,9 @@ def parse_options():
 
 def main():
     inherited_options = parse_options()
-    ref__file = inherited_options.ref__file
+    ref__file = inherited_options.scANVI_ref__file
     # ref__file="/lustre/scratch126/humgen/projects/sc-eqtl-ibd/analysis/freeze_003/ti-cd_healthy-fr003_004/anderson_ti_freeze003_004-eqtl_processed.h5ad"
-    label_col = inherited_options.ref__file
+    label_col = inherited_options.label_col
     # label_col="label__machine"
     outdir = inherited_options.outdir
     # outdir = "/lustre/scratch126/humgen/projects/sc-eqtl-ibd/analysis/bradley_analysis/results/rectum_to_TI/objects"
@@ -107,18 +107,28 @@ def main():
     adata = ad.read_h5ad(ref__file)
     print("Loaded the data")
     print(f"Dataset shape: {adata.shape}")
-    
+
     # Check if GPU is available:
     gpu_use = torch.cuda.is_available()
     print(f"Is a GPU avalable?: {gpu_use}")
-    
+
     # Make sure there is a 'counts' layer for 'X', and this is raw (printing)
     adata.layers['counts'] = adata.X.copy()
-    sparse.csr_matrix(adata.X)[:10, :30].toarray()
-    
+    print(sparse.csr_matrix(adata.X)[:10, :30].toarray())
+
+    # Make sure label_col is in the adata.obs
+    if not label_col in adata.obs.columns:
+        annot = pd.read_csv("/lustre/scratch126/humgen/projects/sc-eqtl-ibd/analysis/bradley_analysis/proc_data/highQC_TI_discovery/data-clean_annotation_full.csv")
+        annot['cluster'] = annot['cluster'].astype('category')
+        adata.obs['cluster'] = adata.obs['cluster'].astype(int)
+        annot['label__machine'] = annot['label__machine_retired'] # Use correct version
+        cells = adata.obs.index
+        adata.obs = adata.obs.merge(annot, on="cluster")
+        adata.obs.index = cells
+        
     # Build scVI model using GPU
     print("~~~~~~~~~~~~~~~~~~~ Batch correcting with scVI - optimum params ~~~~~~~~~~~~~~~~~~~")
-    Trainer(accelerator="cuda")
+    #Trainer(accelerator="cuda")
     scvi.settings.dl_pin_memory_gpu_training =  True
     scvi.model.SCVI.setup_anndata(adata, layer="counts", batch_key=sample_identifier)
     model = scvi.model.SCVI(adata, n_layers=2, n_latent=30, gene_likelihood="nb")
@@ -135,14 +145,15 @@ def main():
         labels_key=label_col,
         unlabeled_category="Unknown",
     )
-    
+        
     #scanvi_model.train(max_epochs=20, n_samples_per_label=100, use_gpu=True)
     scanvi_model.train(max_epochs=20, n_samples_per_label=100)
     SCANVI_LATENT_KEY = "X_scANVI"
     adata.obsm[SCANVI_LATENT_KEY] = scanvi_model.get_latent_representation(adata)
-    
+        
     # Save the scanvi model and the adata object with these reductions
-    adata.write(outdir + "/adata_PCAd_batched.h5ad")
+    adata.obs.drop('cluster', axis=1, inplace=True)
+    adata.write(outdir + "/adata_ref_scANVI.h5ad")
     scanvi_model.save(f"{outdir}/TI_scanvi_model", overwrite=True)
     
 # Execute
