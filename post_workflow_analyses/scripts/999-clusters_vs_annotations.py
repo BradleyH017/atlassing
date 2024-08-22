b49fa35e0204b44d493f4c915c2a49cc4540d575
BradleyH017 <bh18@sanger.ac.uk>
2024-08-22 16:37:48 +0100

    fix bug

---
+++ post_workflow_analyses/scripts/999-clusters_vs_annotations.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+__author__ = 'Bradley Harris'
+__date__ = '2024-08-12'
+__version__ = '0.0.1'
+
+# Load in the libraries
+import scanpy as sc
+import anndata as ad
+import pandas as pd
+import numpy as np
+import argparse
+import os
+from matplotlib import rcParams
+
