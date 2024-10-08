21c3058e060387f6168881e9ecb529d4e7e0c17c
BradleyH017 <bh18@sanger.ac.uk>
2024-10-08 07:57:18 +0100

    add highlighting plots

--- post_workflow_analyses/scripts/999-highqc_dataset_plots.py
+++ post_workflow_analyses/scripts/999-highqc_dataset_plots.py
@@ -23,24 +23,53 @@ sc.settings.set_figure_params(dpi=500, facecolor='white', format="png")
 # Load anndata (backed)
 adata = sc.read('/lustre/scratch126/humgen/projects/sc-eqtl-ibd/analysis/bradley_analysis/scripts/scRNAseq/Atlassing/results_round3/combined/objects/adata_PCAd_batched_umap.h5ad', backed='r')
 
-# Add provisional category annotation
+# Add provisional category annotation and colours
 annots = pd.read_csv("temp/2024-08-28_provisional_annot_cats.csv")
+colours = pd.read_csv("temp/master_colours.csv")
 
 # Bind
 adata.obs.reset_index(inplace=True)
 adata.obs = adata.obs.merge(annots, on="leiden", how="left")
+adata.obs.set_index("cell", inplace=True)
+
+# Define map
+color_map = dict(zip(colours['Category'], colours['Colour code']))
+
 
 # Make overall plots:
 overall_plot = ["tissue", "Category", "Label_1", "disease_status"]
+adata.obs['tissue'].replace('r', 'Rectum', inplace=True)
+adata.obs['tissue'].replace('ti', 'TI', inplace=True)
+adata.obs['tissue'].replace('blood', 'Blood', inplace=True)
 for c in overall_plot:
     print(c)
-    if c == "Category":
-        sc.pl.umap(adata, color=c, legend_loc="on data", frameon=False, title=None, legend_fontoutline=1, legend_fontsize=10, save=f'_overall_{c}.png', show=False)
+    if c in ["Category", "tissue"]:
+        sc.pl.umap(adata, color=c, legend_loc="on data", frameon=False, palette=color_map, title=None, legend_fontoutline=1, legend_fontsize=10, save=f'_overall_{c}.png', show=False)
+        sc.pl.umap(adata, color=c, frameon=False, palette=color_map, title=None, legend_fontsize=10, save=f'_overall_{c}_legend_off.png', show=False)
+    if c == "Label_1":
+        sc.pl.umap(adata, color=c, frameon=False, cmap='Set1', title=None, legend_fontsize=10, save=f'_overall_{c}.png', show=False)
     else: 
         sc.pl.umap(adata, color=c, frameon=False, title=None, legend_fontsize=10, save=f'_overall_{c}.png', show=False)
 
 # Plot within category
 cats = np.unique(adata.obs['Category'])
+#adata.obs = adata.obs.strings_to_categoricals()
+#adata.obs['Label_1'] = adata.obs['Label_1'].astype('category')
+#adata.obs['Label_1'] = adata.obs['Label_1'].astype('category')
 for c in cats:
     print(c)
-    sc.pl.umap(adata[adata.obs['Category'] == c], color="Label_1", frameon=False, title=None, legend_fontsize=10, save=f'_within_Category_{c}.png', show=False)
+    #adata_subset = adata[adata.obs['Category'] == c].copy() 
+    sc.pl.umap(adata[adata.obs['Category'] == c], color="Label_1", cmap='Set1', frameon=False, title=None, legend_fontsize=10, save=f'_within_Category_{c}.png', show=False)
+
+
+# Plot for some specific categories / cell-states (grey out the rest)
+level = ["tissue", "Category", "Label_1", "Category", "Label_1"]
+annot = ["Rectum", "Colonocyte", "Colonocyte (1)", "T", "CD8 tissue resident memory (1)"]
+tissue = ["Rectum", "Rectum", "Rectum", "TI", "TI"]
+cols = ["#E07B28", "#E07B28", "#E07B28", "#D640A1", "#D640A1"]
+for i, l in enumerate(level):
+    adata.obs['plot'] = (adata.obs[level[i]] == annot[i]) & (adata.obs.tissue == tissue[i])
+    adata.obs['plot'] = adata.obs['plot'].replace({False: "No", True: "Yes"})
+    fname = f"{tissue[i]}-{level[i]}-{annot[i]}"
+    temp_cols_map = dict({"Yes": cols[i], "No": "#D3D3D3"})
+    sc.pl.umap(adata, color="plot", frameon=False, palette = temp_cols_map, title=None, legend_fontsize=10, save=f'_{fname}.png', show=False)
