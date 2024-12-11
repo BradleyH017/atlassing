1d2e6e134db5a43c12200649b93b18dc4d3cb6c7
BradleyH017 <bh18@sanger.ac.uk>
2024-12-11 14:11:59 +0000

    add high qc plots without bad clusters

--- post_workflow_analyses/scripts/999-highqc_dataset_plots.py
+++ post_workflow_analyses/scripts/999-highqc_dataset_plots.py
@@ -14,17 +14,86 @@ from sklearn.decomposition import PCA
 import plotnine as plt9
 from matplotlib.lines import Line2D
 
-# Define outdir
-sc.settings.figdir="results_round3/combined/figures/UMAP"
+
+# Make the round 1 lineage annotation plot
+sc.settings.figdir="other_paper_figs"
 sc.settings.verbosity = 3
 sc.logging.print_header()
 sc.settings.set_figure_params(dpi=500, facecolor='white', format="png")
 
+# Load the anndata object from round 1
+r1 = sc.read_h5ad("results_round1/all/tables/clustering_array/leiden_0.03/adata_PCAd_batched_umap_0.03.h5ad")
+umap = r1.obsm['UMAP_X_scVI']
+clusters = r1.obs['leiden']
+del r1
+adata = sc.read_h5ad("input_v7/adata_raw_input_all.h5ad")
+adata = adata[adata.obs.index.isin(clusters.index)]
+adata.obsm['X_umap'] = umap
+adata.obs['leiden'] = clusters
+
+major = {"Epithelial": ["EPCAM" ,'CDH1', 'KRT19'], "Mesenchymal": ["COL1A1","COL1A2","COL6A2","VWF"], 'B':['CD79A', 'MS4A1', 'MS4A1', 'CD79B'], 'Plasma':['MZB1', 'JCHAIN'], 'T_NK':['CD3D', 'CD3E', 'CD3G','CCR7','IL7R', 'TRAC'], 'Myeloid':['ITGAM', 'CD14', 'CSF1R', 'TYROBP'], 'Mast':["TPSAB1", 'TPSB2', "CPA3" ], 'Platelet+RBC':['GATA1', 'TAL1', 'ITGA2B', 'ITGB3', 'GP1BA', 'PPBP', 'PF4', 'TUBB1', 'THBS1']}
+sc.pl.dotplot(adata, major, layer="log1p_cp10k", gene_symbols = "gene_symbols", groupby='leiden', dendrogram=False, save="_r1_major_lineage_markers.png")
+
+# Generate maps of clusters --> manual lineages
+manual_mapping = {"0": "Epithelial", "1": "T-NK", "2": "B", "3": "Myeloid", "4": "B", "5": "Mesenchymal", "6": "Epithelial", "7": "Mast", "8": "Platelet"}
+adata.obs['manual_lineage'] = adata.obs['leiden'].map(manual_mapping)
+
+# Derive colours
+colours = pd.read_csv("temp/master_colours.csv")
+colours['manual_lineage'] = colours['Category'] # Plotting the manual lineages here
+color_map = dict(zip(colours['manual_lineage'], colours['Colour code']))
+
+# Plot UMAPs
+sc.pl.umap(adata, color='manual_lineage', legend_loc="on data", frameon=False, palette=color_map, title=None, legend_fontoutline=1, legend_fontsize=10, save=f'_overall_r1.png', show=False)
+sc.pl.umap(adata, color='manual_lineage', frameon=False, palette=color_map, title=None, legend_fontsize=10, save=f'_overall_r1_legend_off.png', show=False)
+sc.pl.umap(adata, color='leiden', legend_loc="on data", frameon=False, palette='Set2', title=None, legend_fontoutline=1, legend_fontsize=10, save=f'_overall_r1_leiden.png', show=False)
+sc.pl.umap(adata, color='leiden', frameon=False, palette='Set2', title=None, legend_fontsize=10, save=f'_overall_r1_leiden_legend_off.png', show=False)
+
+
+############ Plot results from WITHIN major lineages
+del adata
+lins_res = {"all_Epithelial": 0.9, "all_Mesenchymal": 1.0, "all_Myeloid": 0.7, "all_B": 0.8, "all_T": 1.1, "Platelet+RBC": np.nan, "Mast": np.nan}
+chuck = {"all_Epithelial": ["11", "33", "34"], "all_Mesenchymal": ["15"], "all_Myeloid": ["6", "13", "18"], "all_B": ["14", "15"], "all_T": ["17", "18", "23", "25"]}
+additional_chuck = {"all_Epithelial": ["8"], "all_Mesenchymal": ["10"], "all_B": ["7"], "all_Myeloid": ["12", "5", "15"]}
+chuck_additional = True
+
+linadata = []
+final_clusters = []
+for l in lins_res:
+    print(f"Working on: {l}")
+    res = lins_res[l]
+    if not np.isnan(res):
+        adata = sc.read_h5ad(f"results_round2/{l}/tables/clustering_array/leiden_{str(res)}/adata_PCAd_batched_umap_{str(res)}.h5ad")
+        clusters = adata.obs[['leiden']].reset_index()
+        clusters = clusters[~clusters['leiden'].astype(str).isin(chuck[l])]
+        if chuck_additional and l in additional_chuck.keys():
+            clusters = clusters[~clusters['leiden'].astype(str).isin(additional_chuck[l])]
+        pref = l.replace("all_", "")
+        clusters['leiden'] = pref + "_" + clusters['leiden'].astype(str)
+    else:
+        clusters = pd.DataFrame({"cell": adata.obs[adata.obs['manual_lineage'] == l].index.values, "leiden": l})
+    
+    adata.obs['pref_leiden'] = pref + "_" + adata.obs['leiden'].astype(str)
+    adata = adata[adata.obs.index.isin(clusters['cell'])]
+    linadata.append(adata)
+    final_clusters.append(clusters)
+
+final_clusters = pd.concat(final_clusters)
+
+# Plot
+for index, l in enumerate(lins_res.keys()):
+    print(f"~~~ Plotting {l} ~~~")
+    sc.pl.umap(linadata[index], color='leiden', legend_loc="on data", frameon=False, title=None, legend_fontoutline=1, legend_fontsize=10, save=f'_{l}_good_clusters.png', show=False)
+    sc.pl.umap(linadata[index], color='leiden', frameon=False, title=None, legend_fontoutline=1, legend_fontsize=10, save=f'_{l}_good_clusters_legend_off.png', show=False)
+
+
+
+############ Plot the results from high quality data ACROSS major lineages
 # Load anndata (backed)
 adata = sc.read('/lustre/scratch126/humgen/projects/sc-eqtl-ibd/analysis/bradley_analysis/scripts/scRNAseq/Atlassing/results_round3/combined/objects/adata_PCAd_batched_umap.h5ad', backed='r')
 
 # Add provisional category annotation and colours
-annots = pd.read_csv("temp/2024-08-28_provisional_annot_cats.csv")
+annots = pd.read_csv("temp/2024-12-02_annot_cats.csv") # post annotation jamboree
 colours = pd.read_csv("temp/master_colours.csv")
 
 # Bind
@@ -36,7 +105,7 @@ adata.obs.set_index("cell", inplace=True)
 color_map = dict(zip(colours['Category'], colours['Colour code']))
 
 # Make overall plots:
-overall_plot = ["tissue", "Category", "Label_1", "disease_status"]
+overall_plot = ["tissue", "Category", "JAMBOREE_ANNOTATION", "disease_status"]
 adata.obs['tissue'].replace('r', 'Rectum', inplace=True)
 adata.obs['tissue'].replace('ti', 'TI', inplace=True)
 adata.obs['tissue'].replace('blood', 'Blood', inplace=True)
@@ -58,9 +127,18 @@ cats = np.unique(adata.obs['Category'])
 for c in cats:
     print(c)
     #adata_subset = adata[adata.obs['Category'] == c].copy() 
-    sc.pl.umap(adata[adata.obs['Category'] == c], color="Label_1", cmap='Set1', frameon=False, title=None, legend_fontsize=10, save=f'_within_Category_{c}.png', show=False)
+    sc.pl.umap(adata[adata.obs['Category'] == c], color="JAMBOREE_ANNOTATION", cmap='Set1', frameon=False, title=None, legend_fontsize=10, save=f'_within_Category_{c}.png', show=False)
     # Also plot with the correct colour for this subset
-    sc.pl.umap(adata, color=c, frameon=False, palette=color_map, title=None, legend_fontsize=10, save=f'_overall_{c}_legend_off.png', show=False)
+    #sc.pl.umap(adata, color=c, frameon=False, palette=color_map, title=None, legend_fontsize=10, save=f'_overall_{c}_legend_off.png', show=False)
+
+# Plot within tissue also
+tissues = np.unique(adata.obs['tissue'])
+for t in tissues:
+    print(c)
+    sc.pl.umap(adata[adata.obs['tissue'] == t], color="JAMBOREE_ANNOTATION", cmap='Set1', frameon=False, title=None, legend_fontsize=10, save=f'_label_within_{t}.png', show=False)
+    sc.pl.umap(adata[adata.obs['tissue'] == t], color="tissue", cmap='Set1', frameon=False, title=None, legend_fontsize=10, save=f'_tissue_within_{t}.png', show=False)
+    sc.pl.umap(adata[adata.obs['tissue'] == t], color="Category", cmap='Set1', frameon=False, title=None, legend_fontsize=10, save=f'_Category_within_{t}.png', show=False)
+
 
 # Plot for some specific categories / cell-states (grey out the rest)
 level = ["tissue", "Category", "Label_1", "Category", "Label_1", "tissue", "Label_1", "Category", "Category", "Category", "Label_1"]
@@ -120,26 +198,29 @@ for tissue, group in tissue_groups:
     plt.clf()
 
 # Plot the number of cells from each tissue contributing to each cluster
+ml = np.unique(adata.obs['manual_lineage'])
 col="leiden"
-proportion_df = adata.obs.groupby([col, 'tissue']).size().reset_index(name='count')
-proportion_df['proportion'] = proportion_df.groupby(col)['count'].transform(lambda x: x / x.sum())
-pivot_df = proportion_df.pivot(index=col, columns="tissue", values='proportion').fillna(0)
-color_mapping = {"r": "#E07B28", "blood": "#BB5566", "ti": "#117733"}
-fig, ax = plt.subplots(figsize=(30, 10))
-bottom = np.zeros(len(pivot_df))
-
-for idx, category in enumerate(pivot_df.columns):
-    ax.bar(pivot_df.index, pivot_df[category], bottom=bottom, color=color_mapping.get(category, "#000000"), label=category)
-    bottom += pivot_df[category].fillna(0).values
-
-ax.tick_params(axis='x', rotation=45)
-ax.set_xlim([pivot_df.index.min(), pivot_df.index.max()])
-ax.legend(title=col, bbox_to_anchor=(1.05, 1), loc='upper left')
-ax.set_title('Relative Proportions of Tissue by Cluster (cell number)')
-ax.set_xlabel('Cluster')
-ax.set_ylabel('Proportion')
-plt.savefig(f"results_round3/combined/figures/tissue__across_clusters.png", bbox_inches='tight')
-plt.clf()
+for l in ml:
+    print(f"---- Working on {l} -----")
+    proportion_df = adata.obs[adata.obs['manual_lineage'] == l].groupby([col, 'tissue']).size().reset_index(name='count')
+    proportion_df['proportion'] = proportion_df.groupby(col)['count'].transform(lambda x: x / x.sum())
+    pivot_df = proportion_df.pivot(index=col, columns="tissue", values='proportion').fillna(0)
+    pivot_df = pivot_df[pivot_df.sum(axis=1) > 0]
+    color_mapping = {"r": "#E07B28", "blood": "#BB5566", "ti": "#117733"}
+    fig, ax = plt.subplots(figsize=(15, 8))
+    bottom = np.zeros(len(pivot_df))
+    #
+    for idx, category in enumerate(pivot_df.columns):
+        ax.bar(pivot_df.index, pivot_df[category], bottom=bottom, color=color_mapping.get(category, "#000000"), label=category)
+        bottom += pivot_df[category].fillna(0).values
+    #
+    ax.set_xticklabels(pivot_df.index, rotation=45, ha='right', rotation_mode='anchor')
+    ax.legend(title=col, bbox_to_anchor=(1.05, 1), loc='upper left')
+    ax.set_title('Relative Proportions of Tissue by Cluster (cell number)')
+    ax.set_xlabel('Cluster')
+    ax.set_ylabel('Proportion')
+    plt.savefig(f"results_round3/combined/figures/tissue__across_clusters_{l}.png", bbox_inches='tight')
+    plt.clf()
 
 # Rather, save this as a table. 
 # Cells
