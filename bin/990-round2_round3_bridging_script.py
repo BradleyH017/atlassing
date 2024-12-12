6063c80ef9e8d7d94ac40e824ef10df64eb04a85
BradleyH017 <bh18@sanger.ac.uk>
2024-12-12 13:11:09 +0000

    remove additional clusters

--- bin/990-round2_round3_bridging_script.py
+++ bin/990-round2_round3_bridging_script.py
@@ -8,10 +8,15 @@ import anndata as ad
 import numpy as np
 import pandas as pd
 import os
+chuck_additional = True # Decide whether to chuck the additional chuck clusters or not? (Based on annotation of markers etc)
 
 # Define lineages, desired resolutions and clusters to chuck per lineage
 lins_res = {"all_Epithelial": 0.9, "all_Mesenchymal": 1.0, "all_Myeloid": 0.7, "all_B": 0.8, "all_T": 1.1, "Platelet+RBC": np.nan, "Mast": np.nan}
-chuck = {"all_Epithelial": ["11", "33", "34"], "all_Mesenchymal": ["15"], "all_Myeloid": ["6", "13", "18"], "all_B": ["14", "15"], "all_T": ["17", "18", "23", "25"]}
+chuck = {"all_Epithelial": ["11", "33", "34"], "all_Mesenchymal": ["15"], "all_Myeloid": ["6", "13", "18"], "all_B": ["14", "15"], "all_T": ["17", "18", "23", "25"]} # Removed on the basis of outlying QC or a maximum contribution by single sample > 40% 
+additional_chuck = {"all_Epithelial": ["8"], "all_Mesenchymal": ["10"], "all_B": ["7"], "all_Myeloid": ["12", "9", "5", "15"], "all_T": ["14"]} 
+# Additional were removed on the basis of marker gene expression (either not sufficient for given lineage, or cross-lineage).
+# Myeloid 5 looked like bin for low quality DC cells and Myeloid 15 had outlying QC
+
 
 # Load in the combined output from round1
 adata = sc.read_h5ad("results_round1/all/objects/adata_manual_lineage_clean.h5ad")
@@ -25,6 +30,8 @@ for l in lins_res:
         clusters = pd.read_csv(f"results/{l}/tables/clustering_array/leiden_{str(res)}/clusters.csv")
         clusters.rename(columns={f"leiden_{res}": "leiden"}, inplace=True)
         clusters = clusters[~clusters['leiden'].astype(str).isin(chuck[l])]
+        if chuck_additional and l in additional_chuck.keys():
+            clusters = clusters[~clusters['leiden'].astype(str).isin(additional_chuck[l])]
         pref = l.replace("all_", "")
         clusters['leiden'] = pref + "_" + clusters['leiden'].astype(str)
     else:
@@ -54,6 +61,12 @@ for key, value_list in chuck.items():
     for value in value_list:
         keys.append(key)
         values.append(value)
+
+if chuck_additional:
+    for key, value_list in additional_chuck.items():
+        for value in value_list:
+            keys.append(key)
+            values.append(value)
 
 # Create the DataFrame
 df = pd.DataFrame({
